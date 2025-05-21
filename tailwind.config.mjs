ea735d0b6fa7b60b1af07a7c13b64e9950b48edb
AlanRB08 <alanreyesbueno08@gmail.com>
2025-05-21 14:11:56 -0600

    Actualizacion de navbar y footer, comienzo del landing

---
+++ tailwind.config.mjs
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+      extend: {
+        colors : {
+            'blue_bg' : '#14427C', // Azul de fondos y cards
+            'blue_bg2' : '#0044A4', // Azul de fondos y cards 2
+            'red_bg' : '#CA1C1C', // Rojo de botones e iconos
+            'gris_titulos': '#393939', // Gris obscuro para titulos 
+            'verde_whats': '#128c7e' , // Verde de whatshapp
+            'gris_text' : '#737373' // Gris para menu
+        },
+        fontFamily : {
+            sans: [
+                'Helvetica', 
+                'Arial', 
+                'sans-serif', // Fallback genérico
+              ],
+        }
+      },
+    },
+    plugins: [],
+  };
