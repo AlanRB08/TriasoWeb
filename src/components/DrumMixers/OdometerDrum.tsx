bd2384f070e86744d9b7434f173d4f85512cc8ce
AlanRB08 <alanreyesbueno08@gmail.com>
2025-06-20 14:51:34 -0600

    Se creo la nueva pagina Drum Mixers, se agregaron 3 secciones

---
+++ src/components/DrumMixers/OdometerDrum.tsx
@@ -0,0 +1,97 @@
+import React, { useEffect, useState, useRef } from 'react';
+import Odometer from 'react-odometerjs';
+import 'odometer/themes/odometer-theme-default.css';
+
+const OdometerDrum = () => {
+  const sectionRef = useRef<HTMLDivElement | null>(null);
+
+  const [value, setValue] = useState(0);
+  const [value1, setValue1] = useState(0);
+  const [value2, setValue2] = useState(0);
+  const [value3, setValue3] = useState(0);
+  const [value4, setValue4] = useState(0);
+  const [value6, setValue6] = useState(0);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          // Reinicia valores antes de animar
+          setValue(0);
+          setValue1(0);
+          setValue2(0);
+          setValue3(0);
+          setValue4(0);
+
+          setTimeout(() => {
+            setValue(25);
+            setValue6(500);
+            setValue1(40);
+            setValue2(7);
+            setValue4(5);
+            setValue3(150);
+          }, 300); // Pequeño retraso para asegurar reinicio
+        }
+      },
+      {
+        threshold: 0.5, // cuando el 50% sea visible
+      }
+    );
+
+    const current = sectionRef.current;
+    if (current) observer.observe(current);
+
+    return () => {
+      if (current) observer.unobserve(current);
+    };
+  }, []);
+
+  return (
+    <div ref={sectionRef} className="w-full px-8 lg:px-52 mt-20">
+      <div className="grid grid-cols-1 md:grid-cols-2 justify-center items-center">
+        <div className="flex flex-col items-start justify-center gap-10 md:gap-20">
+          <div className="flex flex-col items-center justify-center">
+            <div className="flex text-7xl font-normal justify-start items-baseline w-full">
+              <Odometer value={value} format="(,ddd)" duration={2000} />
+              <h1>-</h1>
+              <Odometer value={value6} format="(,ddd)" duration={2000} />
+              <p className="text-sm font-normal ml-3">Tph</p>
+            </div>
+            <p className="text-grisP">Range of hot-mix production</p>
+          </div>
+
+          <div className="flex flex-col items-center justify-center">
+            <div className="flex text-7xl font-normal justify-start items-baseline w-full">
+              <Odometer value={value1} format="(,ddd)" duration={2000} />
+              <p className="text-sm font-normal ml-3">%</p>
+            </div>
+            <p className="text-grisP">RAP (Recycled Asphalt Pavement) Incorporation</p>
+          </div>
+
+          <div className="flex flex-col items-center justify-center">
+            <div className="flex text-7xl font-normal justify-start items-baseline w-full">
+              <Odometer value={value2} format="(,ddd)" duration={2000} />
+              <h1>.</h1>
+              <Odometer value={value4} format="(,ddd)" duration={2000} />
+              <h1>-</h1>
+              <Odometer value={value3} format="(,ddd)" duration={2000} />
+              <p className="text-sm font-normal ml-3">million BTU/hr</p>
+            </div>
+            <p className="text-grisP text-start w-full">Modulating burner capacity</p>
+          </div>
+        </div>
+
+        <div className="flex flex-col items-center justify-center">
+          <div>
+            <img src="" alt="" />
+          </div>
+          <button className="border border-black px-3 py-2 rounded-xl hover:bg-slate-300">
+            All Technical Details
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OdometerDrum;
