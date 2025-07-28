9a0abd30dbb097f8dc1eb916b356281bd07031a3
AlanRB08 <alanreyesbueno08@gmail.com>
2025-07-28 12:35:07 -0600

    BHPage Update

--- src/components/BagHouses/BHTab.tsx
+++ src/components/BagHouses/BHTab.tsx
@@ -6,19 +6,19 @@ const images = [
   { id: 1, src: "/Gallery/baghouses2.png", alt: "Imagen 2" },
 ];
 
-export default function Tabs() {
+export default function Tab() {
   const [activeIndex, setActiveIndex] = useState(0);
 
   return (
-    <div className="w-full grid grid-cols-1 lg:grid-cols-2 justify-center items-center gap-8 mt-10 mb-4">
-      <div className="flex lg:flex-col items-stretch justify-around gap-4 md:gap-20">
+    <div className="w-full grid grid-cols-1 lg:grid-cols-2 justify-center items-center gap-8 mt-10 mb-4 md:px-32">
+      <div className="flex lg:flex-col items-stretch justify-around gap-4 md:gap-10">
         {images.map((img, index) => (
           <button
             aria-label="Select tab"
             key={index}
             onClick={() => setActiveIndex(index)}
-            className={`text-center md:text-start font-normal border-b-2 md:pr-4 md:w-8/12 text-sm md:text-base ${
-              activeIndex === index ? "border-red-500 text-black" : "border-transparent"
+            className={`text-center md:text-start font-normal p-4 rounded-xl md:pr-4 md:w-8/12 text-sm md:text-base ${
+              activeIndex === index ? "border-red-500 text-white bg-blueMain" : "border-transparent bg-white text-grisT"
             }`}
           >
             {index === 0 ? (
@@ -31,12 +31,16 @@ export default function Tabs() {
             )}
           </button>
         ))}
+        <p className="font-bold text-grisT text-base md:text-lg">
+        Self-contained, integrated dust control systems for effective air filtration and particle capture in asphalt plants—configurable 
+        for both portable and stationary setups.
+        </p>
       </div>
 
       <img
         src={images[activeIndex].src}
         alt={images[activeIndex].alt}
-        className="border border-blueMain rounded-2xl h-[350px] w-full"
+        className="border-2 border-blueMain rounded-2xl h-[350px] w-full"
       />
     </div>
   );
