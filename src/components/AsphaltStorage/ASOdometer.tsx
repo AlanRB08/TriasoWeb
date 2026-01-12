c55ee2912e9f797fcdd99fd1731e3da0f40ca2f3
AlanRB08 <alanreyesbueno08@gmail.com>
2026-01-12 17:16:07 -0600

    Rap & Bin Units Buttons

--- src/components/AsphaltStorage/ASOdometer.tsx
+++ src/components/AsphaltStorage/ASOdometer.tsx
@@ -94,6 +94,31 @@ const ASOdometer = () => {
           <div>
             <img src={ASOdom.src} alt="" />
           </div>
+          <a
+  href="#planosAsphaltStorage"
+  className="
+    group relative inline-flex items-center justify-center
+    px-4 py-2 rounded-xl
+    border border-black
+    text-black font-medium
+    overflow-hidden
+
+    transition-all duration-300 ease-out
+    hover:text-white hover:-translate-y-0.5 hover:shadow-lg
+  "
+>
+  <span
+    className="
+      absolute inset-0 bg-black
+      translate-y-full
+      transition-transform duration-300 ease-out
+      group-hover:translate-y-0
+    "
+  />
+  <span className="relative z-10">
+    All Technical Details
+  </span>
+</a>
         </div>
       </div>
     </div>
