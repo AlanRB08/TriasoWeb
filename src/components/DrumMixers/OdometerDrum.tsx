807ea9ce0f520f803c40c6e1c54a18ed3cfb05d5
EduMA13 <monarrez.edu.isw@unipolidgo.edu.mx>
2026-01-29 13:43:15 -0600

    Cambios Odometer drum mix

--- src/components/DrumMixers/OdometerDrum.tsx
+++ src/components/DrumMixers/OdometerDrum.tsx
@@ -49,11 +49,11 @@ const OdometerDrum = () => {
   }, []);
 
   return (
-    <div ref={sectionRef} className="max-w-7xl mx-auto px-8 lg:mt-28 lg:mb-80 md:mt-28 md:mb-80">
+    <div ref={sectionRef} className="max-w-7xl mx-auto px-8 lg:mt-56 lg:mb-56 md:mt-56 md:mb-56">
       <div className="grid grid-cols-1 md:grid-cols-2 justify-center items-center">
         <div className="flex flex-col items-center lg:items-start justify-center gap-10 md:gap-20">
           <div className="flex flex-col items-center justify-center lg:items-start">
-            <div className="flex text-6xl lg:text-7xl font-normal justify-center items-center lg:justify-start lg:items-baseline w-full">
+            <div className="flex text-6xl lg:text-[5rem] md:text-[5rem] font-normal justify-center items-center lg:justify-start lg:items-baseline w-full">
               <Odometer value={value} format="(,ddd)" duration={2000} />
               <h1>-</h1>
               <Odometer value={value6} format="(,ddd)" duration={2000} />
@@ -63,7 +63,7 @@ const OdometerDrum = () => {
           </div>
 
           <div className="flex flex-col items-center justify-center">
-            <div className="flex text-6xl lg:text-7xl font-normal justify-center items-center lg:justify-start lg:items-baseline w-full">
+            <div className="flex text-6xl lg:text-[5rem] md:text-[5rem] font-normal justify-center items-center lg:justify-start lg:items-baseline w-full">
               <Odometer value={value1} format="(,ddd)" duration={2000} />
               <p className="text-sm font-normal ml-3">%</p>
             </div>
@@ -73,7 +73,7 @@ const OdometerDrum = () => {
           </div>
 
           <div className="flex flex-col items-center justify-center">
-            <div className="flex text-6xl lg:text-7xl font-normal justify-center items-center lg:justify-start lg:items-baseline w-full gap-1 lg:gap-0">
+            <div className="flex text-6xl lg:text-[5rem] md:text-[5rem] font-normal justify-center items-center lg:justify-start lg:items-baseline w-full gap-1 lg:gap-0">
               <Odometer value={value2} format="(,ddd)" duration={2000} />
               <h1>.</h1>
               <Odometer value={value4} format="(,ddd)" duration={2000} />
